f3e1b3e268f739e97ac51429dc51bd3dded4e469
b4ljk <baljinnyam.dayan@gmail.com>
2022-04-16 20:58:23 +0800

    huge update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import AppRouter from './components/AppRouter'
-import AuthContextProvider from './contexts/AuthContext'
+import React from "react";
+import AppRouter from "./components/AppRouter";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App(props) {
   return (
     <AuthContextProvider>
       <AppRouter />
     </AuthContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
