5e942f119a3811cdcfb70a14404aeba1cc4f528f
b4ljk <baljinnyam.dayan@gmail.com>
2022-04-24 18:11:54 +0800

    404 page

--- src/pages/NotfoundPage.jsx
+++ src/pages/NotfoundPage.jsx
@@ -1,11 +1,46 @@
-import { Heading } from "@chakra-ui/react";
-import React from "react";
-import { Layout } from "../components/Layout";
+import { Box, Heading, Text, Button, Link } from "@chakra-ui/react";
+import SmallWithLogoLeft from "../components/Footer";
+import { Navbar } from "../components/Navbar";
 
 export default function NotfoundPage() {
   return (
-    <Layout>
-      <Heading>Not found page</Heading>
-    </Layout>
+    <Box
+      h={"100vh"}
+      display={"flex"}
+      flexDir="column"
+      justifyContent={"space-between"}
+    >
+      <Box textAlign="center" py={1} px={6}>
+        <Navbar />
+        <Box h={"20vh"}></Box>
+        <Heading
+          display="inline-block"
+          as="h2"
+          size="2xl"
+          bgGradient="linear(to-r, teal.400, teal.600)"
+          backgroundClip="text"
+        >
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Ийм хуудас олдсонгүй
+        </Text>
+        <Text color={"gray.500"} mb={6}>
+          Андуурал гарааааааааааааюуууудаааааа ?? үгүй бол бидэнд мэдэгдээрэй 👌
+        </Text>
+
+        <Link href="/">
+          <Button
+            colorScheme="teal"
+            bgGradient="linear(to-r, teal.400, teal.500, teal.600)"
+            color="white"
+            variant="solid"
+          >
+            нүүр хуудасруу буцах
+          </Button>
+        </Link>
+      </Box>
+      <SmallWithLogoLeft />
+    </Box>
   );
 }
